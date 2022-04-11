d11f8ccc3dfbcccac20c556dee0c49fc45205a67
Christiano0407 <placebo0704@gmail.com>
2022-04-11 10:36:21 -0500

    photo api add unsplash

--- buildPhotoAPI/photoAPI.js
+++ buildPhotoAPI/photoAPI.js
@@ -1,1 +1,6 @@
 //* ==  Project API  Photography == */
+const apiKey = ` 68wlXxaMmNtUzrADXqGtupAgDMr3IQEtkb9lPwWhETY`;
+const searchInput = document.querySelector(".search--input");
+const viewMore = document.querySelector(".view-More");
+const searchForm = document.querySelector(".search__form");
+const mainContent = document.querySelector(".main");
