4a0ba7dd8a963589158ef6193aed25bcbdde4b7f
Christiano0407 <placebo0704@gmail.com>
2022-04-11 11:34:54 -0500

    part 02

--- buildPhotoAPI/photoAPI.js
+++ buildPhotoAPI/photoAPI.js
@@ -1,6 +1,36 @@
 //* ==  Project API  Photography == */
-const apiKey = ` 68wlXxaMmNtUzrADXqGtupAgDMr3IQEtkb9lPwWhETY`;
+const apiKey = ` 68wlXxaMmNtUzrADXqGtupAgDMr3IQEtkb9lPwWhETY`; // Token>
 const searchInput = document.querySelector(".search--input");
 const viewMore = document.querySelector(".view-More");
 const searchForm = document.querySelector(".search__form");
 const mainContent = document.querySelector(".main");
+let fetchLink;
+let currentSearch;
+let searchValue;
+let pageNumber = 1;
+
+//* ===> apiHeaders  02 <==  */
+const apiHandling = async (url) => {
+  const fetchData = await fetch(url, {
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+      Authorization: apiKey,
+    },
+  });
+
+  const data = await fetchData.json();
+  return data;
+};
+
+//* == Get Curate Photos 01  == */
+const curatePhotos = async () => {
+  /*  const fetchData = await fetch("https://api.pexels.com/v1/curated?per_page=15"); */
+  /*  console.log(fetchData.json()); */
+  const data = await apiHandling();
+  /*  `https://api.pexels.com/v1/curated?per_page=10` */
+  console.log(data);
+  /*  generateMarkup(data); */
+};
+
+curatePhotos();
